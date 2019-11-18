fd5fd6ef9797497ac56a1d2646b1daa1810c43d2
Chanakya <chanakya.valluri@gmail.com>
2019-11-18 11:56:25 -0800

    mongo connection

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
-
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
 app.get("/", (req, res) => res.send("Hello World"));
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
 
