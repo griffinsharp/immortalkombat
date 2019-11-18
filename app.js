cc098f53166f6b9a9e49e5154bb92bec52c65f0b
Chanakya <chanakya.valluri@gmail.com>
2019-11-18 11:47:45 -0800

    run both servers

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
