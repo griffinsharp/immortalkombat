5ff5376b1042ded0183add94cd5794e58aeac847
Bob Andre <bppa1905@gmail.com>
2019-11-18 10:48:57 -0800

    finish signup and login forms

--- frontend/components/login.jsx
+++ frontend/components/login.jsx
@@ -1,30 +1,51 @@
 import React, { Component } from 'react'
 
-export default class login extends Component {
-
+export default class Login extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             username: '',
-            password: ''
+            password: '',
+            loginErrors: ''
         }
-        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleSubmit(e) {
-        e.preventDefault();
-        // need to hit the right routes with a POST api call 
+    handleSubmit(event) {
+        event.preventDefault();
+        const user = this.state;
+        //NEED TO CALL THE RIGHT API ENDPOINT
     }
+
+    handleChange(event) {
+
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
 
     render() {
         return (
-            <>
-                <form action="">
-                    <input type="text" placeholder='username' name='username' />
-                    <input type="password" placeholer='password' name='password' />
-                    <input type="submit" name='submit' onClick={this.handleSubmit} />
+            <React.Fragment>
+                <form onSubmit={this.handleSubmit}>
+
+                    <input type="username"
+                        name="username"
+                        placeholder="username"
+                        defaultValue={this.state.username} onChange={this.handleChange} required />
+
+                    <input type="password"
+                        name="password"
+                        placeholder="password"
+                        defaultValue={this.state.passowrd} onChange={this.handleChange} required />
+
+                    <button type='submit' className='btn btn-flat'>
+                        Login
+                        </button>
                 </form>
-            </>
+            </React.Fragment>
         )
     }
 }
