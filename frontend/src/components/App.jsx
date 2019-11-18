0a6f673f488d0ef9eca093750ada8f6338d808c2
Bob Andre <bppa1905@gmail.com>
2019-11-18 14:26:19 -0800

    home css

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,35 +1,25 @@
-import React from "react";
+import React from "react"
 import Game from './Game'
 import Home from './Home'
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
+    <>
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/game">Game</Link>
-            </li>
-          </ul>
-        </nav>
-
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/game">
+          <Route exact path="/game">
             <Game />
           </Route>
           <Route path="/">
             <Home />
           </Route>
         </Switch>
-      </div>
+      
     </Router>
+    </>
   );
 }
 
