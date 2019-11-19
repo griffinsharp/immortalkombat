39ea2213353316a01c78ad579fcd02f230e56e84
Bob Andre <bppa1905@gmail.com>
2019-11-18 17:03:30 -0800

    small fix

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -6,16 +6,13 @@ import Game from './Game'
 import Home from './Home'
 import Login from './auth/Login'
 import Signup from './auth/Signup'
-import { AuthRoute, ProtectedRoute } from '../util/route_util.jsx.js'
+import { AuthRoute, ProtectedRoute } from '../util/route_util'
 
 function App() {
   return (
     <>
     <Router>
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
-
           <ProtectedRoute Route exact path="/game" component={Game} />
           <AuthRoute exact path="/signup" component={Signup} />
           <AuthRoute exact path="/login" component={Login} />
