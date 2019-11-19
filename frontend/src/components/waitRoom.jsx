fdda461377c7002480e800cabac43b0a23118f98
Chanakya <chanakya.valluri@gmail.com>
2019-11-19 09:47:42 -0800

    baisc connection established

--- frontend/src/components/waitRoom.jsx
+++ frontend/src/components/waitRoom.jsx
@@ -1,6 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import * as io from "socket.io-client";
 
 export default class WaitRoom extends Component {
+
+    componentDidMount() {
+        this.socket = io.connect("http://localhost:5000");
+        this.socket.on("welcome", (data) => {
+            console.log("Received: ", data);
+        });
+    }
+
     render() {
         return (
             <div className="waitroom-container">
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const mongoose = require("mongoose");
@@ -10,14 +12,19 @@ mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully!"))
     .catch(err => console.log(err));
-    
+
+io.on("connection", (socket) => {
+    socket.emit("welcome", "You are connected.");
+});
+
 app.use(passport.initialize());
 require("./config/passport")(passport);
-    
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+server.listen(port, () => console.log(`Server is running on port ${port}`));
+// app.listen(port, () => console.log(`Server is running on port ${port}`));
