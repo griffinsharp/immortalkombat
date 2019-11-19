03094752e73516b2814c0824cdba9fc6c8474e5b
Bob Andre <bppa1905@gmail.com>
2019-11-18 18:33:29 -0800

    added navbar compo(undone) and code input

--- frontend/src/components/profile.jsx
+++ frontend/src/components/profile.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+//components
+import Navbar from './Navbar'
 
 export default function profile() {
     return (
-        <div>
-            
+        <>
+        <div className='container'>
+           <Navbar /> 
+           <input type="text" placeholder='code' name='code' />
+           <button className='btn btn-flat'>Go!</button>
         </div>
+        </>
     )
 }
 
