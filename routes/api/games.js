d4944413c1cfcf56c153c88a159ab53356cd6cac
Bob Andre <bppa1905@gmail.com>
2019-11-20 14:02:53 -0800

    api post for game, return json game

--- routes/api/games.js
+++ routes/api/games.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken");
 const passport = require("passport");
 const keys = require("../../config/keys");
-const User = require("../../models/Game");
+const Game = require("../../models/Game");
 
 router.post('/',(req,res)=>{
-
-    const newGame = new Games({
+    passport.authenticate('jwt', { session: false });
+    const newGame = new Game({
         winner: req.body.winner,
         loser: req.body.loser, 
         score: req.body.score
-
     });
+    newGame.save().then((game) => res.json(game))
 })
