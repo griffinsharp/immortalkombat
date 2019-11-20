a27bffced5f8cd487f2214740f30de2baa74219e
Chanakya <chanakya.valluri@gmail.com>
2019-11-20 10:12:33 -0800

    add game room to state

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import session from "./session_reducer";
+import game from "./game_reducer";
 import errors from "./errors_reducer";
 
 const RootReducer = combineReducers({
     session,
+    game,
     errors
 });
 
