3393e487400eaf320ee130d11739f5bca3e38e11
Bob Andre <bppa1905@gmail.com>
2019-11-25 02:00:28 -0800

    style

--- frontend/src/components/stats/stats.jsx
+++ frontend/src/components/stats/stats.jsx
@@ -32,18 +32,35 @@ export default class stats extends Component {
             <div className='container'>
                     <h1>{this.state.username}</h1>
                     <h2>highscore: {this.state.highscore}</h2>
-                <ol>
-                    winner loser time winnerHit LoserHit
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Winner</th>
+                                <th>Loser</th>
+                                <th>Time</th>
+                                <th>WinnerHit</th>
+                                <th>LoserHit</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
                     {this.state.stats.map((stat) => {
-                        return (<li>
-                            {stat.winner} 
-                            {stat.loser} 
-                            {stat.time} 
-                            {stat.winnerHitPercentage} 
-                            {stat.loserHitPercentage}
-                            </li>)
+                        return (
+                            <tr>
+                                <td>{stat.winner}</td>
+                                <td>{stat.loser}</td>
+                                <td>{stat.time}</td>
+                                <td>{stat.winnerHitPercentage}</td>
+                                <td>{stat.loserHitPercentage}</td>
+                            </tr> 
+                                )
                     })}
-                </ol>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                            </tr>
+                        </tfoot>
+                    </table>
                 <hr/>
                 <hr/>
                 
@@ -52,3 +69,4 @@ export default class stats extends Component {
         )
     }
 }
+
