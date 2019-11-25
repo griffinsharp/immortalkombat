decaaa30e4afa49315db7623805643e4af54385d
Ricardo de Arruda <settiricardo@gmail.com>
2019-11-25 00:44:32 -0800

    add vibration for controller

--- frontend/src/components/controller/controller.jsx
+++ frontend/src/components/controller/controller.jsx
@@ -1,101 +1,132 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import * as io from "socket.io-client";
-import Repeatable from 'react-repeatable';
+import Repeatable from "react-repeatable";
 
 export default class Controller extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: this.props.game.room,
+      username: this.props.currentUser.username,
+      id: this.props.currentUser.id,
+      vibration: false,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            code: this.props.game.room,
-            username: this.props.currentUser.username,
-            id: this.props.currentUser.id
-        }
+  componentDidMount() {
+    if (this.state.code === undefined) {
+      this.props.history.push("/play");
+    } else {
+      let hostname =
+        window.location.hostname === "localhost"
+          ? "http://localhost:5000/games"
+          : `https://${window.location.hostname}/games`;
+      this.socket = io.connect(hostname);
+      this.socket.emit("joinRoom", JSON.stringify(this.state));
+      this.socket.on("newUser", res => {
+        let data = JSON.parse(res);
+        console.log(data.msg);
+      });
+      // this.socket.on("message", msg => console.log(msg));
     }
+  }
 
-    componentDidMount() {
-        if (this.state.code === undefined) {
-            this.props.history.push('/play')
-        } else {
-            let hostname = window.location.hostname === 'localhost' ? 
-            'http://localhost:5000/games' : `https://${window.location.hostname}/games`
-            this.socket = io.connect(hostname);
-            this.socket.emit("joinRoom", JSON.stringify(this.state));
-            this.socket.on("newUser", res => {
-                let data = JSON.parse(res);
-                console.log(data.msg);
-            });
-            // this.socket.on("message", msg => console.log(msg));
-        }
+  createAction(action) {
+    return JSON.stringify({
+      action: action,
+      username: this.state.username,
+      id: this.state.id,
+      room: this.state.code,
+    });
+  }
+
+  vibration(action) {
+    let that = this;
+    if (!this.vibration) {
+      this.setState({ vibrate: true });
+
+      switch (action) {
+        case "left":
+          window.navigator.vibrate(100);
+          setTimeout(() => that.setState({ vibrate: false }), 100);
+          break;
+        case "right":
+          window.navigator.vibrate(100);
+          setTimeout(() => this.setState({ vibrate: false }), 100);
+          break;
+        case "hammer":
+          window.navigator.vibrate(700);
+          setTimeout(() => this.setState({ vibrate: false }), 700);
+          break;
+        case "jump":
+          window.navigator.vibrate(100);
+          setTimeout(() => this.setState({ vibrate: false }), 100);
+          break;
+
+        default:
+          break;
+      }
     }
+  }
 
-    createAction(action) {
-        return JSON.stringify({
-            action: action,
-            username: this.state.username,
-            id: this.state.id,
-            room: this.state.code
-        });
-    }
+  setAction(action) {
+    this.socket.emit("message", this.createAction(action));
+    this.vibration(action);
+  }
 
-    setAction(action) {        
-        this.socket.emit("message", this.createAction(action));
-    }
+  renderButton(btnClassName, action) {
+    return (
+      <Repeatable
+        className={btnClassName}
+        repeatDelay={0}
+        repeatInterval={10}
+        onPress={() => this.setAction(action)}
+        onHoldStart={() => this.setAction(action)}
+        onHold={() => this.setAction(action)}
+        onHoldEnd={() => this.setAction("")}
+        onRelease={() => this.setAction("")}
+      />
+    );
+  }
 
-    renderButton(btnClassName, action) {
-        return (
-					<Repeatable
-                        className={btnClassName}
-                        repeatDelay={0}
-                        repeatInterval={10}
-						onPress={() => this.setAction(action)}
-						onHoldStart={() => this.setAction(action)}
-						onHold={() => this.setAction(action)}
-						onHoldEnd={() => this.setAction("")}
-						onRelease={() => this.setAction("")}
-					/>
-				);
-    }
+  render() {
+    return (
+      <div className="controller-container">
+        <div className="cable"></div>
+        <div className="controller">
+          <div className="centerBlue">
+            <div className="centerLeft"></div>
+            <div className="centerRight"></div>
+          </div>
+          <div className="centerStart">
+            <div className="SLeft"></div>
+            <div className="SRight"></div>
+          </div>
+          <div className="centerSelect">
+            <div className="SLeft"></div>
+            <div className="SRight"></div>
+          </div>
 
-
-    render() {
-        return (
-            <div className='controller-container'>
-                <div className='cable'></div>
-                <div className='controller'>
-                    <div className='centerBlue'>
-                        <div className='centerLeft'></div>
-                        <div className='centerRight'></div>
-                    </div>
-                    <div className='centerStart'>
-                        <div className='SLeft'></div>
-                        <div className='SRight'></div>
-                    </div>
-                    <div className='centerSelect'>
-                        <div className='SLeft'></div>
-                        <div className='SRight'></div>
-                    </div>
-
-                    <div className='controllerLeft'>
-                        <div className='circle'></div>
-                        <div className='crossCenter'>
-                            {this.renderButton('crossLeft', 'left')}
-                            {this.renderButton('crossRight', 'right')}
-                            <div className='crossCircle'></div>
-                        </div>
-                    </div>
-                    <div className='controllerRight'>
-                        <div className='backButton1Center'>
-                            {this.renderButton('cornerLeft1', 'jump')}
-                            {this.renderButton('cornerRight1', 'hammer')}
-                        </div>
-                        {/* <div className='backButton2Center'>
+          <div className="controllerLeft">
+            <div className="circle"></div>
+            <div className="crossCenter">
+              {this.renderButton("crossLeft", "left")}
+              {this.renderButton("crossRight", "right")}
+              <div className="crossCircle"></div>
+            </div>
+          </div>
+          <div className="controllerRight">
+            <div className="backButton1Center">
+              {this.renderButton("cornerLeft1", "jump")}
+              {this.renderButton("cornerRight1", "hammer")}
+            </div>
+            {/* <div className='backButton2Center'>
                             <div className= 'cornerLeft2'></div>
                             <div className= 'cornerRight2'></div>
                         </div> */}
-                    </div>
-                </div>
-            </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
