d1a9912e95ae66388c2fa1631bae1e223f39d6c0
Chanakya <chanakya.valluri@gmail.com>
2019-11-25 09:52:57 -0800

    redirect to / after sign up

--- frontend/src/components/auth/signup_form.jsx
+++ frontend/src/components/auth/signup_form.jsx
@@ -20,7 +20,7 @@ class SignupForm extends React.Component {
 
 	componentWillReceiveProps(nextProps) {
 		if (nextProps.signedIn === true) {
-			this.props.history.push("/profile");
+			this.props.history.push("/");
 		}
 
 		this.setState({ errors: nextProps.errors });
